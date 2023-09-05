1f05b38641eafde00d406b5c891242896dd5588c
sewastian <sebastianrincon834@gmail.com>
2023-09-05 16:25:13 -0500

    feat [libs -> initialSetup.js]

---
+++ libs/initialSetup.js
@@ -0,0 +1,25 @@
+const {Admin} = require('../models/admin')
+
+
+const rolAdmin ={
+    createAdmin : async () =>{
+    try{
+        const count = await Admin.estimatedDocumentCount()
+
+        if (count > 0 ) return
+
+        const values = await Promise.all([
+            new Admin({name: "user"}).save(),
+            new Admin({name: "admin"}).save()
+        ])
+
+    console.log(values)
+    }catch ( error){
+        console.error(error)
+    }
+},
+
+}
+
+module.exports = rolAdmin.createAdmin
+
