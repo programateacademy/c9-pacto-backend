45d028bc2258cee6266d222f3d71d565de2cdd3f
sewastian <sebastianrincon834@gmail.com>
2023-09-05 16:23:36 -0500

    feat [databse.js]

---
+++ database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const app = require('./app')
+require(`dotenv`).config()
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_DB_URI) // connect in mongoDB
+    .then(()=>
+    console.log("Conexion to DB"))
+    .catch((err)=>
+    console.error(err));
+
+    app.get('/',(req,res) => {
+    res.send('Connected')
+})
+
