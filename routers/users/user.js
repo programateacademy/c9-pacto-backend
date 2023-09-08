1c82b9d2a13754a6a184d7e8b2e198de6b20431a
sewastian <sebastianrincon834@gmail.com>
2023-09-07 23:44:33 -0500

    feat [routers/users -> user.js]

---
+++ routers/users/user.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const controllerUser = require('../../controllers/users/user')
+
+const router = express.Router()
+const authJwt = require('../../middlewares/authJwt')
+const verifySignup = require('../../middlewares/verifySignup')
+
+
+router.post('/create',[ authJwt.verifyToken,authJwt.isAdmin,verifySignup.checkRoleExist,verifySignup.checkDupletUserNameOrEmail
+],controllerUser.create)
+
+
+router.get('/', controllerUser.getUser)
+router.get('/:id', controllerUser.getUserById)
+router.patch('/update/:id', controllerUser.updateUser)
+router.delete('/delete/:id',[ authJwt.verifyToken,authJwt.isAdmin,verifySignup.checkRoleExist
+], controllerUser.deleteUser)
+
+module.exports = router
