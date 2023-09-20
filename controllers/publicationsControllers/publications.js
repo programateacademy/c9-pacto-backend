bf28b07bbbd82e302584292355c714d88660170f
sewastian <sebastianrincon834@gmail.com>
2023-09-20 12:54:40 -0500

    feat [controllers -> publications.js]

--- controllers/publicationsControllers/publications.js
+++ controllers/publicationsControllers/publications.js
@@ -1,28 +1,38 @@
-const Publication = require('../../models/PublicationsModels/publications');
-const User = require('../../models/users/user');
+const Publication = require('../../models/publicationsModels/publications');
+const User = require('../../models/user');
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId;
+
+
 
 const controllerPublication = {
     create: async (req, res) => {
         try {
-            const {userId,description,image} = req.body
+            const { userId, description, image, likes} = req.body
             const date_create = new Date();
 
+            console.log("UserID:", userId)
+
             const user = await User.findById(userId) // buscar el ID del usuario
-            if (!user){
-                return res.status(404).json({error: "could not find user"})
+            if (!user) {
+                return res.status(404).json({ error: "could not find user" })
             }
 
-            await Publication.create({
+            const newPublication = await Publication.create({
                 user: user._id,
                 date_create: date_create,
                 description: description,
                 image: image,
+                likes: []
             });
-            res.json({ msg: 'created' });
+            newPublication.likes = likes.map((userId) => mongoose.Types.ObjectId(userId));
+            await newPublication.save()
+            res.json({ msg: 'publication created', publication: newPublication });
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
     },
+
     getPublication: async (req, res) => {
         try {
             const publications = await Publication.find({})
@@ -31,6 +41,7 @@ const controllerPublication = {
             return res.status(500).json({ msg: error.message })
         }
     },
+
     getPublicationById: async (req, res) => {
         try {
             const { id } = req.params
@@ -59,6 +70,7 @@ const controllerPublication = {
             })
         }
     },
+
     deletePublication: async (req, res) => {
         try {
             const { id } = req.params
@@ -67,7 +79,90 @@ const controllerPublication = {
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
+    },
+
+    // Dar like a una publicación
+    likePublication: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { userId } = req.body;
+
+            // Verifica si userId es un ObjectId válido
+            if (!ObjectId.isValid(userId)) {
+                return res.status(400).json({ error: 'userId no es válido' });
+            }
+
+            console.log(userId)
+
+            const publication = await Publication.findById(id);
+
+            if (!publication) {
+                return res.status(404).json({ error: 'Publicación no encontrada' });
+            }
+
+
+            if (publication.likes.includes(userId)) {
+                return res.status(400).json({ error: 'El usuario ya ha dado like a esta publicación' });
+            }
+
+            publication.likes.push(userId);
+            await publication.save();
+            console.log('Likes después de dar like:', publication.likes.length); // Agrega esta línea
+
+            res.json({ msg: 'Like agregado correctamente', publication });
+        } catch (error) {
+            return res.status(500).json({ msg: 'Error al dar like a la publicación', error: error.message });
+        }
+    },
+
+    // Quitar like a una publicación
+    unlikePublication: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { userId } = req.body;
+
+            // Verifica si userId es un ObjectId válido
+            if (!ObjectId.isValid(userId)) {
+                return res.status(400).json({ error: 'userId no es válido' });
+            }
+
+            console.log(userId)
+
+            const publication = await Publication.findById(id);
+
+            if (!publication) {
+                return res.status(404).json({ error: 'Publicación no encontrada' });
+            }
+
+            if (!publication.likes.includes(userId)) {
+                return res.status(400).json({ error: 'El usuario no ha dado like a esta publicación' });
+            }
+
+            publication.likes = publication.likes.filter(user => user.toString() !== userId);
+            await publication.save();
+            console.log('Likes después de quitar like:', publication.likes.length); // Agrega esta línea
+            res.json({ msg: 'Like quitado correctamente', publication });
+        } catch (error) {
+            return res.status(500).json({ msg: 'Error al quitar like a la publicación', error: error.message });
+        }
+    },
+
+    getLikesForUser: async (req, res) => {
+        try {
+            const { userId } = req.params;
+
+            // Realiza una búsqueda de las publicaciones que le gustan al usuario
+            const likedPublications = await Publication.find({ likes: userId });
+
+            // Extrae los identificadores de las publicaciones que le gustan al usuario
+            const likedPublicationIds = likedPublications.map((publication) => publication._id);
+
+            res.json(likedPublicationIds);
+        } catch (error) {
+            return res.status(500).json({ msg: 'Error al obtener los likes del usuario', error: error.message });
+        }
     }
+
 }
 
 module.exports = controllerPublication
