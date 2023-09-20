bec87d777aee166cc982604ad3c17bbbc6426e2c
sewastian <sebastianrincon834@gmail.com>
2023-09-20 12:55:10 -0500

    update [libs -> initialSetup.js]

--- libs/initialSetup.js
+++ libs/initialSetup.js
@@ -1,50 +1,56 @@
-const {Admin} = require ('../models/users/admin')
-const User = require('../models/users/user')
-const config = require ('../config')
-
+const {Admin} = require('../models/admin')
+const User = require('../models/user')
+const config = require('../config')
 
 const rolAdmin ={
     createAdmin : async () =>{
-    try{
+    try{    
         const count = await Admin.estimatedDocumentCount()
 
         if (count > 0 ) return
 
+        //creacion de los Roles
         const values = await Promise.all([
             new Admin({name: "user"}).save(),
             new Admin({name: "admin"}).save()
         ])
-
+    
     console.log(values)
     }catch ( error){
         console.error(error)
     }
 },
-adminprint : async () => {
-  try {
+
+    adminprint : async () => {
+        try {
 
 
-      const userFound = await User.findOne({ email: config.ADMIN_EMAIL });
-      console.log(userFound);
+            const userFound = await User.findOne({ email: config.ADMIN_EMAIL });
+            console.log(userFound);
 
-      if (userFound) return;
+            if (userFound) return;
+
+            
+            const admin = await Admin.find({name: {$in:["admin"]}})
+
+            // Concatenate ADMIN_NAMES and ADMIN_SURNAMES to create userName
+            const userName = `${config.ADMIN_NAMES} ${config.ADMIN_SURNAMES}`;
 
 
-      const admin = await Admin.find({name: {$in:["admin"]}})
+            const userRegis = await User.create({
+                names: config.ADMIN_NAMES,
+                surNames: config.ADMIN_SURNAMES,
+                userName,            
+                email: config.ADMIN_EMAIL,
+                password: config.ADMIN_PASSWORD,
+                admin: admin.map((admins) =>admins._id)
+            });
 
-      const userRegis = await User.create({
-          userName: config.ADMIN_USERNAME,
-          email: config.ADMIN_EMAIL,
-          password: config.ADMIN_PASSWORD,
-          admin: admin.map((admins) =>admins._id)
-      });
-
-      console.log(`New user created: ${userRegis.email}`);
-  } catch (error) {
-      console.log(error);
-  }
-}
-
+            console.log(`New user created: ${userRegis.email}`);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 module.exports = rolAdmin
