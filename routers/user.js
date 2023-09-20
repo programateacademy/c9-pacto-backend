67d09f39234a89b134652f03571f89ec34427872
sewastian <sebastianrincon834@gmail.com>
2023-09-20 12:58:10 -0500

    update [routers -> user.js]

---
+++ routers/user.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const controllerUser = require('../controllers/user')
+
+const router = express.Router()
+const authJwt = require('../middlewares/authJwt')
+const verifySignup = require('../middlewares/verifySignup')
+
+
+router.post('/create',[ authJwt.verifyToken,authJwt.isAdmin,verifySignup.checkRoleExist,verifySignup.checkDupletUserNameOrEmail
+],controllerUser.create) // registro de usuarios que solo puede realizar admin
+
+
+router.get('/', controllerUser.getUser)
+router.get('/:id', controllerUser.getUserById)
+router.delete('/delete/:id',[ authJwt.verifyToken,authJwt.isAdmin,verifySignup.checkRoleExist
+], controllerUser.deleteUser) // eliminar usuarios que solo puede realizar admin
+
+module.exports = router
