97910dc028c75d578aafb03275306ab085eb82df
sewastian <sebastianrincon834@gmail.com>
2023-09-23 16:57:55 -0500

    update [routers -> user.js]

--- routers/user.js
+++ routers/user.js
@@ -13,6 +13,6 @@ router.post('/create',[ authJwt.verifyToken,authJwt.isAdmin,verifySignup.checkRo
 
 router.get('/', controllerUser.getUser)
 router.get('/:id', controllerUser.getUserById)
-router.delete('/delete/:id',authJwt.isAdmin, controllerUser.deleteUser) // eliminar usuarios que solo puede realizar admin
+router.delete('/delete/:id',controllerUser.deleteUser) // eliminar usuarios que solo puede realizar admin
 
 module.exports = router
