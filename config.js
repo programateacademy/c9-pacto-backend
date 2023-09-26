7e14ae5c5e9a43ec6eb1cdc5e9af059ab0eb3b68
sewastian <sebastianrincon834@gmail.com>
2023-09-26 15:33:53 -0500

    update [config.js]

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports ={
-    SECRET: "poofo123secretclave456",
+    SECRET: "poofo123secretclave456", //agregar SECRET, palabra secreta para el manejo de los token
 
-    ADMIN_EMAIL : "johanrincon404@gmail.com",
+    ADMIN_EMAIL : "johanrincon404@gmail.com", //correo de la persona o entidad  que hace en el envio para recuperar contraseña
 
-    ADMIN_NAMES: "Johan Sebastian",
+    ADMIN_NAMES: "Johan Sebastian", // nombre del administrador 
 
-    ADMIN_SURNAMES:"Beltran Rincon",
+    ADMIN_SURNAMES:"Beltran Rincon", // apellidos del administrador
 
-    ADMIN_PASSWORD : "mqkr qgjp wiyi ikur",
+    ADMIN_PASSWORD : "mqkr qgjp wiyi ikur", // contraseña generada por gmail para envio de correos
 
-    URL: "https://c9-pacto-front.vercel.app/" //URL = http://localhost:3000
+    URL: "https://c9-pacto-front.vercel.app/" //URL = http://localhost:4200
 
 }
